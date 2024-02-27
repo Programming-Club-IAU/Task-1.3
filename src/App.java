3bc61a9422c4dee5fb41f25a3d9a4e33becde7f3
H4ya <h4yathegenius@gmail.com>
2024-02-27 22:29:33 +0300

    DONE

--- src/App.java
+++ src/App.java
@@ -5,25 +5,41 @@ public class App {
         int option=0; double temp, result;
         System.out.println("Welcome to our Temperature Converter program!");
         while(option!=3){
-            System.out.println("Choose an option:\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\n3. Exit\n\nEnter your option:");
+            System.out.print("\nChoose an option:\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\n3. Exit\n\nEnter your option:");
             option = sc.nextInt();
             switch(option){
                 case 1:
                     System.out.print("Enter the temperature in Celsius:");
-                    temp = nextDouble();
+                    temp = sc.nextDouble();
                     result = (temp*9/5)+32;
-                    System.out.println("The temperature in Fahrenheit is: "+result);
+                    System.out.println(" _______________________________________________");
+                    System.out.println("(   )                                         /");
+                    System.out.println(" | |    The temperature in Fahrenheit is:");
+                    System.out.println(" | |\t\t"+result+"°F");
+                    System.out.println("(___)_________________________________________\\");
                     break;
-                case2:
+                case 2:
                     System.out.print("Enter the temperature in Fahrenheit:");
-                    temp = nextDouble();
+                    temp = sc.nextDouble();
                     result = (temp-32)*5/9;
-                    System.out.println("The temperature in Celsius is: "+result);
+                    System.out.println(" _______________________________________________");
+                    System.out.println("(   )                                         /");
+                    System.out.println(" | |    The temperature in Celsius is:");
+                    System.out.println(" | |\t\t"+result+"°C");
+                    System.out.println("(___)_________________________________________\\");
                     break;
-                case3:
-                    System.out.println("It was a pleasure to serve you. Goodbye 0v0!");
+                case 3:
+                    System.out.println(" _______________________________________________");
+                    System.out.println("(   )                                         /");
+                    System.out.println(" | |\tIt was a pleasure to serve you.");
+                    System.out.println(" | |\t\t Goodbye 0v0!");
+                    System.out.println("(___)_________________________________________\\");
                     break;
-
+                default:        
+                System.out.println(" (\\__/)");
+                System.out.println(" ( -.-)\tInvalid option. Please try again.");
+                System.out.println("@(\")(\")");
+            }
         }
     }
 }
