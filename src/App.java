4772d47ced91155559e2a2c573d388f562c4d435
Haya <145580738+H4ya@users.noreply.github.com>
2024-02-27 21:06:50 +0300

    h

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello, World!");
     }
 }
