b443fa0430679089c21ee3513f36ce4fd668a608
Mohmmad-IAU <2240005057@iau.edu.sa>
2024-02-28 12:18:15 +0300

    Good#

--- convert_temperatures.java
+++ convert_temperatures.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
+
 public class convert_temperatures {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Scanner scanner2 = new Scanner(System.in);
-    boolean X = true; 
-    int input ; Double Fahrenheit ,Celsius;
-    while(X){
-        System.out.println(
-        "---------------Welcome---------------"+
-                       "1- Celsius to Fahrenheit\r\n" + 
-                       "2- Fahrenheit to Celsius\r\n" + 
-                       "3- Exit\n" + 
-                       "Enter your choice:\r");
-    input = scanner.nextInt();
-     switch(input) {
-     case input = 3 :
-     System.out.println("Exiting the program. Goodbye!");
-     X = false ;
-     break;
-     case input = 2 :
-     System.out.println("Enter the temperature in Fahrenheit: ");
-     Fahrenheit = scanner2.nextDouble();
-     Celsius = ( Fahrenheit - 32 ) * 5/9;
-     System.out.println(Fahrenheit+"°F is equal to "+Celsius+"°C");
-     break;
-     case input = 3 :
-     System.out.println("Enter the temperature in Celsius: ");
-     Celsius = scanner2.nextDouble();
-     Fahrenheit = (Celsius * 9)/5 + 32 ;
-     System.out.println(Celsius+"°C is equal to "+Fahrenheit+"°F");
-     break;
-     default:
-     System.out.println("\t\t\n\n--- Input error ---");
-     } 
-     scanner.close();
-     scanner2.close();
-     }
+        Scanner scanner = new Scanner(System.in);
+        boolean X = true;
+        int input;
+        Double Fahrenheit, Celsius;
+        while (X) {
+            System.out.println(
+                    "---------------Welcome---------------\n" +
+                            "1- Celsius to Fahrenheit\r\n" +
+                            "2- Fahrenheit to Celsius\r\n" +
+                            "3- Exit\n" +
+                            "Enter your choice:\r");
+            input = scanner.nextInt();
+            switch (input) {
+                case 3:
+                    System.out.println("Exiting the program. Goodbye!");
+                    X = false;
+                    break;
+                case 2:
+                    System.out.println("Enter the temperature in Fahrenheit: ");
+                    Fahrenheit = scanner.nextDouble();
+                    Celsius = (Fahrenheit - 32) * 5 / 9;
+                    System.out.println(Fahrenheit + "°F is equal to " + Celsius + "°C");
+                    break;
+                case 1:
+                    System.out.println("Enter the temperature in Celsius: ");
+                    Celsius = scanner.nextDouble();
+                    Fahrenheit = (Celsius * 9) / 5 + 32;
+                    System.out.println(Celsius + "°C is equal to " + Fahrenheit + "°F");
+                    break;
+                default:
+                    System.out.println("\t\t\n\n--- Input error ---\n\n");
+            }
+        }
+        scanner.close();
     }
 }
